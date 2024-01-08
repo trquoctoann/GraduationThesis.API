4804dddd18b6367b4783c3de3c32d60a39e6aa2f
Quoc Toan Tran <quoctoan09072002@gmail.com>
2024-01-08 13:39:33 +0700

    initial version of pizza talk

---
+++ src/test/java/com/cheems/pizzatalk/PizzaTalkApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cheems.pizzatalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaTalkApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
