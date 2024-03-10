cb5ec4fb5b66b89e89b40145bed2f17f523e7d9f
Quoc Toan Tran <quoctoan09072002@gmail.com>
2024-03-10 23:47:25 +0700

    feat(role, permission): add app's default role and permission

--- src/main/java/com/cheems/pizzatalk/security/PermissionConstants.java
+++ src/main/java/com/cheems/pizzatalk/security/PermissionConstants.java
@@ -4,5 +4,64 @@ public final class PermissionConstants {
 
     private PermissionConstants() {}
 
-    public static final String SYSTEM = "System";
+    public static final String SYSTEM = "SYSTEM";
+
+    public static final String AREA_CREATE = "AREA.CREATE";
+    public static final String AREA_READ = "AREA.READ";
+    public static final String AREA_UPDATE = "AREA.UPDATE";
+    public static final String AREA_DELETE = "AREA.DELETE";
+
+    public static final String STORE_CREATE = "STORE.CREATE";
+    public static final String STORE_READ = "STORE.READ";
+    public static final String STORE_UPDATE = "STORE.UPDATE";
+    public static final String STORE_DELETE = "STORE.DELETE";
+
+    public static final String CATEGORY_CREATE = "CATEGORY.CREATE";
+    public static final String CATEGORY_READ = "CATEGORY.READ";
+    public static final String CATEGORY_UPDATE = "CATEGORY.UPDATE";
+    public static final String CATEGORY_DELETE = "CATEGORY.DELETE";
+
+    public static final String DEAL_CREATE = "DEAL.CREATE";
+    public static final String DEAL_READ = "DEAL.READ";
+    public static final String DEAL_UPDATE = "DEAL.UPDATE";
+    public static final String DEAL_DELETE = "DEAL.DELETE";
+
+    public static final String OPTION_CREATE = "OPTION.CREATE";
+    public static final String OPTION_READ = "OPTION.READ";
+    public static final String OPTION_UPDATE = "OPTION.UPDATE";
+    public static final String OPTION_DELETE = "OPTION.DELETE";
+
+    public static final String OPTION_DETAIL_CREATE = "OPTION_DETAIL.CREATE";
+    public static final String OPTION_DETAIL_READ = "OPTION_DETAIL.READ";
+    public static final String OPTION_DETAIL_UPDATE = "OPTION_DETAIL.UPDATE";
+    public static final String OPTION_DETAIL_DELETE = "OPTION_DETAIL.DELETE";
+
+    public static final String PERMISSION_READ = "PERMISSION.READ";
+    public static final String PERMISSION_ASSIGN = "PERMISSION.ASSIGN";
+
+    public static final String PRODUCT_CREATE = "PRODUCT.CREATE";
+    public static final String PRODUCT_READ = "PRODUCT.READ";
+    public static final String PRODUCT_UPDATE = "PRODUCT.UPDATE";
+    public static final String PRODUCT_DELETE = "PRODUCT.DELETE";
+
+    public static final String ROLE_CREATE = "ROLE.CREATE";
+    public static final String ROLE_READ = "ROLE.READ";
+    public static final String ROLE_UPDATE = "ROLE.UPDATE";
+    public static final String ROLE_DELETE = "ROLE.DELETE";
+    public static final String ROLE_ASSIGN = "ROLE.ASSIGN";
+
+    public static final String USER_READ = "USER.READ";
+    public static final String USER_UPDATE = "USER.UPDATE";
+    public static final String USER_DELETE = "USER.DELETE";
+
+    public static final String ORDER_CREATE = "ORDER.CREATE";
+    public static final String ORDER_READ = "ORDER.READ";
+    public static final String ORDER_UPDATE = "ORDER.UPDATE";
+    public static final String ORDER_DELETE = "ORDER.DELETE";
+    public static final String ORDER_UPDATE_STATUS = "ORDER.UPDATE_STATUS";
+
+    public static final String CART_CREATE = "CART.CREATE";
+    public static final String CART_READ = "CART.READ";
+    public static final String CART_UPDATE = "CART.UPDATE";
+    public static final String CART_DELETE = "CART.DELETE";
 }
--- src/main/java/com/cheems/pizzatalk/security/RoleConstants.java
+++ src/main/java/com/cheems/pizzatalk/security/RoleConstants.java
@@ -4,15 +4,7 @@ public final class RoleConstants {
 
     public RoleConstants() {}
 
-    public static final String ADMINISTRATOR = "Administrator";
+    public static final String ADMINISTRATOR = "ADMINISTRATOR";
 
-    public static final String MANAGER = "Manager";
-
-    public static final String CRAWLER = "Crawler";
-
-    public static final String Employee = "Employee";
-
-    public static final String USER = "User";
-
-    public static final String ANONYMOUS = "Anonymous";
+    public static final String USER = "USER";
 }
