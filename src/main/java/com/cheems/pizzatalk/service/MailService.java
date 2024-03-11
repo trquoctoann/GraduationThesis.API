132a1ce88412fc4391c0126a530eb681354db19a
Quoc Toan Tran <quoctoan09072002@gmail.com>
2024-03-11 16:09:59 +0700

    feat(account): add activation & reset mail's html

--- src/main/java/com/cheems/pizzatalk/service/MailService.java
+++ src/main/java/com/cheems/pizzatalk/service/MailService.java
@@ -1,11 +1,15 @@
 package com.cheems.pizzatalk.service;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -22,25 +26,37 @@ public class MailService {
 
     private final JavaMailSender javaMailSender;
 
-    public MailService(JavaMailSender javaMailSender) {
+    private final ResourceLoader resourceLoader;
+
+    public MailService(JavaMailSender javaMailSender, ResourceLoader resourceLoader) {
         this.javaMailSender = javaMailSender;
+        this.resourceLoader = resourceLoader;
     }
 
     @Async
-    public void sendEmail(String to, String subject, String content) {
+    public void sendEmail(String to, String subject, String content, Boolean isHtml) {
         log.debug("Send email to {}, with subject: {} and content: {}", to, subject, content);
 
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try {
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, StandardCharsets.UTF_8.name());
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, StandardCharsets.UTF_8.name());
             message.setTo(to);
             message.setFrom(this.sourceMail);
             message.setSubject(subject);
-            message.setText(content);
+            message.setText(content, isHtml);
+
+            Resource logo = resourceLoader.getResource("classpath:logo.png");
+            message.addInline("logo", logo);
+
             javaMailSender.send(mimeMessage);
             log.debug("Sent email to {}", to);
         } catch (MailException | MessagingException e) {
             log.warn("Email could not be send to {}", to, e);
         }
+    }
+
+    public String loadHtmlContent(String resourcePath) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + resourcePath);
+        return new String(Files.readAllBytes(resource.getFile().toPath()), StandardCharsets.UTF_8);
     }
 }
